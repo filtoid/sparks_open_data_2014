ae768a9c12eb02ada18870f5dab47effb58a9b47
filtoid <philjeffes@hotmail.com>
2014-02-22 15:22:43 +0000

    Forgot to add these files in these previous check-in

--- site/index.php
+++ site/index.php
@@ -3,13 +3,15 @@
 <head>
 	<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 	<script type="text/javascript" src="js/connect.js"></script>
+	<script type="text/javascript" src="js/convert.js"></script>
+	<script type="text/javascript" src="js/jscoord-1.1.1.js"></script>
 </head>
 
 <body onload="loaded();">
 <h1>Sparks Open Data Event</h1>
 <br>
-
-<img src="http://maps.googleapis.com/maps/api/staticmap?center=42.5351,56.5136&zoom=11&size=200x200&sensor=false">
+<!-- 54.98010777115042, -1.6038523304540302-->
+<img src="http://maps.googleapis.com/maps/api/staticmap?center=54.98010777115042,-1.6038523304540302&zoom=14&size=200x200&sensor=false">
 
 
 </body>
--- site/js/connect.js
+++ site/js/connect.js
@@ -1,3 +1,14 @@
 function loaded(){
 	// Called when the doc has finished loading
+	var gridref = new Object();
+	gridref.northing = 565136;
+	gridref.easting = 425351;
+	//var os = new OsGridRef(425351,565136);
+	
+	var os1 = new OSRef(gridref.easting, gridref.northing);
+	//document.write("OS Grid Reference: " + os1.toString() + " - " + os1.toSixFigureString() + "<br />");
+	var ll1 = os1.toLatLng();
+	//document.write("Converted to Lat/Long: " + ll1.toString());
+	
+     
 }
