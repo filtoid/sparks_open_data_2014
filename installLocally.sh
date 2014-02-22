4f4e0a1a3a52ab69dcbb4b7d06abb4ad30f75b04
filtoid <philjeffes@hotmail.com>
2014-02-22 13:55:50 +0000

    Added in the initial page and a copy script for deploying locally

---
+++ installLocally.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp -R ~/Documents/sparks/sparks_open_data_2014/site/* /var/www/spark
+
---
+++ site/index.php
@@ -0,0 +1,11 @@
+<! doctype html>
+<html>
+<head>
+<script src="https://github.com/filtoid/sparks_open_data_2014.git"></script>
+</head>
+
+<body>
+Empty page
+
+</body>
+</html>
