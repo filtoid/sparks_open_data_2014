92036084cba7f5ddea916f09f1dd8b4076d29ceb
filtoid <philjeffes@hotmail.com>
2014-02-22 17:19:11 +0000

    Added in some map code - get's data and parses it out. not using it yet.

--- site/index.php
+++ site/index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript" src="js/connect.js"></script>
 	<script type="text/javascript" src="js/jscoord-1.1.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="js/leaflet.css" />
+<script type="text/javascript" src="js/leaflet.js"></script>
+<script type="text/javascript" src="js/leafletembed.js"></script>
 </head>
 
 <body onload="loaded();">
 <h1>Sparks Open Data Event</h1>
 <br>
-<!-- 54.98010777115042, -1.6038523304540302-->
-<img src="http://maps.googleapis.com/maps/api/staticmap?center=54.98010777115042,-1.6038523304540302&zoom=14&size=200x200&sensor=false">
-
-
+<!-- 54.98010777115042, -1.6038523304540302
+<img src="http://maps.googleapis.com/maps/api/staticmap?center=54.98010777115042,-1.6038523304540302&zoom=14&size=200x200&sensor=false">-->
+<div id="output"></div>
+ <div id="map"></div>
 </body>
 </html>
--- site/js/connect.js
+++ site/js/connect.js
@@ -1,6 +1,43 @@
 function loaded(){
 	// Called when the doc has finished loading
-	var gridref = new Object();
+	
+	var temp = btoa("sparks_event_phil:Yb2klicnjr^Y");
+	$.getJSON( "getjson.php", function( json ) {
+		
+		var output = new Array();
+		
+		var ret = eval(json);
+		for(var i=0;i<ret.length;i++){
+			var coords = ret[i].definitions[0].point;
+			
+			var os1 = new OSRef(coords.easting, coords.northing);
+			var ll1 = os1.toLatLng();
+			output[i].point = ll1;
+			output[i].name = ret[i].definitions[0].longDescription;
+			//output += "<img src=\"http://maps.googleapis.com/maps/api/staticmap?center=" + ll1.lat +"," + ll1.lng+"&zoom=14&size=200x200&sensor=false&key=AIzaSyADV_yN8ZE8qx-pkpzNEbFSwCvMfW4hpxM\">";
+		}
+		
+		//$("#output").html(output);
+		
+		var map = L.map('map').setView([output.point.lat, output.point.lng], 13);
+		
+		//alert("test");	//console.log( "JSON Data: " + json.users[ 3 ].name );
+	});
+	
+}
+
+
+
+/*	OLD CODE
+	  data: '{"username": "' + username + '", "password" : "' + password + '"}',
+
+  beforeSend: function(xhr){
+				xhr.setRequestHeader("Authentication", "Basic " + encodeBase64( "sparks_event_phil:Yb2klicnjr^Y"));
+			},
+		  
+
+
+var gridref = new Object();
 	gridref.northing = 565136;
 	gridref.easting = 425351;
 	//var os = new OsGridRef(425351,565136);
@@ -9,6 +46,4 @@ function loaded(){
 	//document.write("OS Grid Reference: " + os1.toString() + " - " + os1.toSixFigureString() + "<br />");
 	var ll1 = os1.toLatLng();
 	//document.write("Converted to Lat/Long: " + ll1.toString());
-	
-     
-}
+	*/
